80bdbae6a8d7126d60764634b9a76ffc988e0761
aavdhar <aavdh6@gmail.com>
2022-09-17 16:18:20 -0500

    bld.sh

---
+++ bld.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+export OCLIBS=$OCBUILD/lib
+echo "Building findEye"
+g++ -g -std=c++11 \
+        -I$OC/include/opencv2 \
+        -I$OC/modules/objdetect/include \
+        -I$OC/modules/core/include \
+        -I$OCBUILD \
+        -I$OC/modules/highgui/include \
+        -I$OC/modules/imgcodecs/include \
+        -I$OC/modules/videoio/include \
+        -I$OC/modules/imgproc/include \
+        -I$OC/modules/flann/include \
+        -I$OC/modules/features2d/include \
+        -o findEye \
+        findEyeCenter.cpp \
+        findEyeCorner.cpp \
+        main.cpp \
+        helpers.cpp \
+        -L$OCLIBS \
+        -lopencv_highgui -lopencv_objdetect -lopencv_imgcodecs -lopencv_videoio -lopencv_imgproc -lopencv_core
+
+echo "Building DrawOCV"
+g++ -g -std=c++11 \
+        -I$OC/include/opencv2 \
+        -I$OC/modules/objdetect/include \
+        -I$OC/modules/core/include \
+        -I$OCBUILD \
+        -I$OC/modules/highgui/include \
+        -I$OC/modules/imgcodecs/include \
+        -I$OC/modules/videoio/include \
+        -I$OC/modules/imgproc/include \
+        -I$OC/modules/flann/include \
+        -I$OC/modules/features2d/include \
+        -o drawOcv \
+        drawocv.cpp \
+        -L$OCLIBS \
+        -lopencv_highgui -lopencv_objdetect -lopencv_imgcodecs -lopencv_videoio -lopencv_imgproc -lopencv_core
+
